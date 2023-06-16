a93a1987c6acd1c60850cc68ff5655a48b9dc764
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-06-16 20:17:00 -0300

    feat: ensure sign up controller returns an error if no email is provided

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -2,9 +2,21 @@ export class SignUpController {
   constructor() {}
 
   handle(httpRequest: any) {
-    return {
-      statusCode: 400,
-      body: new Error("Missing parameter: name")
-    };
+    if (!httpRequest.body.name)
+      return {
+        statusCode: 400,
+        body: new Error("Missing parameter: name")
+      };
+
+    if (!httpRequest.body.email)
+      return {
+        statusCode: 400,
+        body: new Error("Missing parameter: email")
+      };
+
+      return {
+        statusCode: 500,
+        body: new Error("Internal server error")
+      };
   }
 }
