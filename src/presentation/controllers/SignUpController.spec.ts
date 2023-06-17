4b889b2979184ce38d40a3b15dda9994bcf7a8c9
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-06-17 11:54:47 -0300

    test: make sign up controller return success response

--- src/presentation/controllers/SignUpController.spec.ts
+++ src/presentation/controllers/SignUpController.spec.ts
@@ -7,6 +7,21 @@ import {
 import { SignUpController } from "./SignUpController";
 
 describe("SignUp Controller", () => {
+  it("should successfully handle request", () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: "Test Name",
+        email: "test@email.com",
+        password: "test1234"
+      }
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toBe("ok");
+  });
+
   it("should return 400 if no name is provided", () => {
     const sut = new SignUpController();
     const httpRequest = {
