9134aad6b897d9b20b73fff4f381bfc811103e0d
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-06-17 13:10:54 -0300

    refactor: make http response helper use controller protocol

--- src/presentation/helpers/HttpResponseHelper.ts
+++ src/presentation/helpers/HttpResponseHelper.ts
@@ -1,11 +1,11 @@
-import { HttpResponse } from "../protocols";
+import { Controller } from "../protocols";
 
 export abstract class HttpResponseHelper {
-  public static ok(body: any): HttpResponse {
-    return new HttpResponse(200, body);
+  public static ok(body: any): Controller.Response {
+    return new Controller.Response(200, body);
   }
 
-  public static badRequest(error: Error): HttpResponse {
-    return new HttpResponse(400, error);
+  public static badRequest(error: Error): Controller.Response {
+    return new Controller.Response(400, error);
   }
 }
