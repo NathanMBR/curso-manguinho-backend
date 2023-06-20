28333db72974b9d1afbb0a8e9111fdd69faa49bf
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-06-20 18:49:58 -0300

    test: make db add account data use case call add account repository

--- src/data/usecases/DbAddAccount.spec.ts
+++ src/data/usecases/DbAddAccount.spec.ts
@@ -6,11 +6,16 @@ import {
 } from "@jest/globals";
 
 import { DbAddAccount } from "./DbAddAccount";
-import { Encrypter } from "../protocols";
+import {
+  AddAccountRepository,
+  Encrypter
+} from "../protocols";
 
 interface GetSUTEnvironmentResponse {
-  SUT: DbAddAccount,
-  encrypter: Encrypter.Protocol
+  encrypter: Encrypter.Protocol;
+  addAccountRepository: AddAccountRepository.Protocol;
+
+  SUT: DbAddAccount;
 }
 
 const getSUTEnvironment = (): GetSUTEnvironmentResponse => {
@@ -20,11 +25,29 @@ const getSUTEnvironment = (): GetSUTEnvironmentResponse => {
     }
   }
 
+  class AddAccountRepositoryStub implements AddAccountRepository.Protocol {
+    async add(account: AddAccountRepository.Request) {
+      return Promise.resolve(
+        {
+          id: "test_id",
+          name: "Test Name",
+          email: "test@email.com",
+          password: "test1234"
+        }
+      );
+    }
+  }
+
   const encrypter = new EncrypterStub();
-  const addAccount = new DbAddAccount(encrypter);
+  const addAccountRepository = new AddAccountRepositoryStub();
+  const addAccount = new DbAddAccount(
+    encrypter,
+    addAccountRepository
+  );
 
   return {
     encrypter,
+    addAccountRepository,
 
     SUT: addAccount
   };
@@ -44,6 +67,31 @@ describe("DbAddAccount UseCase", () => {
     await SUT.add(accountData);
 
     expect(encryptSpy).toHaveBeenCalledWith(accountData.password);
+  });
+
+  it("should pass account data with hashed password to add account repository call", async () => {
+    const {
+      SUT,
+      addAccountRepository,
+      encrypter
+    } = getSUTEnvironment();
+
+    const addSpy = jest.spyOn(addAccountRepository, "add");
+    const accountData = {
+      name: "Test Name",
+      email: "test@email.com",
+      password: "test1234"
+    };
+
+    await SUT.add(accountData);
+
+    const hashedPassword = await encrypter.encrypt(accountData.password);
+    expect(addSpy).toHaveBeenCalledWith(
+      {
+        ...accountData,
+        password: hashedPassword
+      }
+    );
   });
 
   it("should not have a 'try/catch' statement", async () => {
