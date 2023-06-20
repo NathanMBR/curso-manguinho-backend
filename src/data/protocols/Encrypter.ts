7b5f9cc86a0dd1adb2d2cb4bc06c5197eb1579e6
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-06-20 16:36:09 -0300

    feat: add encrypter data protocol

---
+++ src/data/protocols/Encrypter.ts
@@ -0,0 +1,8 @@
+export namespace Encrypter {
+  export type Request = string;
+  export type Response = Promise<string>
+
+  export interface Protocol {
+    encrypt(value: Encrypter.Request): Encrypter.Response;
+  }
+}
