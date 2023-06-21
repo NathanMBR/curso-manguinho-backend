3aaf1aaf53187e958162dfb8e7f8faf994ef17c3
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-06-21 09:22:01 -0300

    feat: add bcrypt adapter at infra layer

---
+++ src/infra/cryptography/BcryptAdapter.ts
@@ -0,0 +1,14 @@
+import bcrypt from "bcrypt";
+
+import { Encrypter } from "../../data/protocols";
+
+export class BcryptAdapter implements Encrypter.Protocol {
+  constructor(
+    private readonly rounds: number
+  ) {}
+
+  async encrypt(value: string) {
+    const hashedValue = await bcrypt.hash(value, this.rounds);
+    return hashedValue;
+  }
+}
