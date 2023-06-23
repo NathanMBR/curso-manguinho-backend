5d65c6e0d6c1da42919c909c5208a7cbf5cf38b4
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-06-23 16:28:20 -0300

    test: ensure cors is used

---
+++ src/main/app/cors.test.ts
@@ -0,0 +1,34 @@
+import { FastifyInstance } from "fastify";
+import supertest from "supertest";
+import {
+  describe,
+  beforeAll,
+  afterAll,
+  it
+} from "@jest/globals";
+
+import { createApp } from "./createApp";
+
+let app: FastifyInstance;
+let request: supertest.SuperTest<supertest.Test>; 
+
+describe("CORS Test", () => {
+  beforeAll(async () => {
+    app = await createApp();
+    request = supertest(app.server);
+
+    app.get("/test", (_request, response) => response.send());
+  
+    await app.listen();
+  });
+  
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should enable access from any origin", async () => {
+    await request
+      .get("/test")
+      .expect("access-control-allow-origin", "*");
+  });
+})
