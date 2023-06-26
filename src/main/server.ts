73e00cd747de9f977ef8d4508275f09a8d624cd9
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-06-26 13:20:01 -0300

    feat: ensure database is online before starting server

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,10 @@
 import { createApp } from "./app";
 import { PORT } from "./config";
+import { prisma } from "../infra/db";
 
 const startServer = async (port: number) => {
+  await prisma.$connect();
+
   const app = await createApp();
 
   const address = await app.listen(
