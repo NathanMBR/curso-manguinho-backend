f4efd7baf5e2dff5545896eedc1007ae40ab4886
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-06-27 20:12:02 -0300

    refactor: update pino logger adapter to reflect interface changes

--- src/utils/pino/PinoLoggerAdapter.ts
+++ src/utils/pino/PinoLoggerAdapter.ts
@@ -2,7 +2,7 @@ import { Logger } from "../../presentation/protocols";
 import { pinoInstance } from "./pinoInstance";
 
 export class PinoLoggerAdapter implements Logger.Protocol {
-  logError(text: string) {
-    pinoInstance.error(text);
+  logError(payload: Logger.Request) {
+    pinoInstance.error(payload);
   };
 }
