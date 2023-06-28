ed77769f2c6a239782cf0db1ff27d4abbda16e3a
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-06-28 10:34:28 -0300

    test: add db find one account by email use case

---
+++ src/data/usecases/DbFindOneAccountByEmail.spec.ts
@@ -0,0 +1,110 @@
+import {
+  describe,
+  it,
+  expect,
+  jest
+} from "@jest/globals";
+
+import { DbFindOneAccountByEmail } from "./DbFindOneAccountByEmail";
+import { FindOneAccountByEmailRepository } from "../protocols";
+import { FindOneAccountByEmail } from "../../domain/usecases";
+
+interface GetSUTEnvironmentResponse {
+  findOneAccountByEmailRepository: FindOneAccountByEmailRepository.Protocol;
+
+  SUT: DbFindOneAccountByEmail;
+}
+
+const getSUTEnvironment = (): GetSUTEnvironmentResponse => {
+  class FindOneAccountByEmailRepositoryStub implements FindOneAccountByEmailRepository.Protocol {
+    findOneByEmail(_search: FindOneAccountByEmail.Request): FindOneAccountByEmail.Response {
+      const account = {
+        id: "test_id",
+        name: "Test Name",
+        email: "test@email.com",
+        password: "test1234"
+      };
+
+      return Promise.resolve(account);
+    }
+  }
+
+  const findOneAccountByEmailRepository = new FindOneAccountByEmailRepositoryStub();
+
+  const SUT = new DbFindOneAccountByEmail(
+    findOneAccountByEmailRepository
+  );
+
+  return {
+    findOneAccountByEmailRepository,
+
+    SUT
+  };
+};
+
+describe("DbFindOneAccountByEmail UseCase", () => {
+  it("should successfully find an account by email", async () => {
+    const { SUT } = getSUTEnvironment();
+
+    const searchData = {
+      email: "search@email.com"
+    };
+    const SUTResponse = await SUT.findOneByEmail(searchData);
+
+    expect(SUTResponse).toEqual(
+      {
+        id: "test_id",
+        name: "Test Name",
+        email: "test@email.com",
+        password: "test1234"
+      }
+    );
+  });
+
+  it("should return null if account isn't found", async () => {
+    const { SUT, findOneAccountByEmailRepository } = getSUTEnvironment();
+
+    jest.spyOn(findOneAccountByEmailRepository, "findOneByEmail").mockImplementationOnce(
+      async () => Promise.resolve(null)
+    );
+
+    const searchData = {
+      email: "search@email.com"
+    };
+    const SUTResponse = await SUT.findOneByEmail(searchData);
+    expect(SUTResponse).toBeNull();
+  });
+
+  it("should pass email to find one account by email repository call", async () => {
+    const { SUT, findOneAccountByEmailRepository } = getSUTEnvironment();
+
+    const findOneByEmailSpy = jest.spyOn(findOneAccountByEmailRepository, "findOneByEmail");
+
+    const searchData = {
+      email: "search@email.com"
+    };
+    await SUT.findOneByEmail(searchData);
+
+    expect(findOneByEmailSpy).toHaveBeenCalledWith(
+      {
+        email: searchData.email
+      }
+    );
+  });
+
+  it("should repass find one account by email repository errors to upper level", async () => {
+    const { SUT, findOneAccountByEmailRepository } = getSUTEnvironment();
+
+    jest.spyOn(findOneAccountByEmailRepository, "findOneByEmail").mockImplementationOnce(
+      async () => {
+        throw new Error();
+      }
+    );
+
+    const searchData = {
+      email: "search@email.com"
+    };
+    const SUTResponse = SUT.findOneByEmail(searchData);
+    await expect(SUTResponse).rejects.toThrow();
+  });
+});
