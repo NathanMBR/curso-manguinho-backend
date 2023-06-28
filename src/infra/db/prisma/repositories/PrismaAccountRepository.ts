31b1bd508bfe43a050784e8f5738bf855ccc3e62
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-06-28 10:39:16 -0300

    feat: add find one by email method at prisma account repository

--- src/infra/db/prisma/repositories/PrismaAccountRepository.ts
+++ src/infra/db/prisma/repositories/PrismaAccountRepository.ts
@@ -1,7 +1,13 @@
-import { AddAccountRepository } from "../../../../data/protocols";
+import {
+  AddAccountRepository,
+  FindOneAccountByEmailRepository
+} from "../../../../data/protocols";
 import { prisma } from "../prisma";
 
-export class PrismaAccountRepository implements AddAccountRepository.Protocol {
+export class PrismaAccountRepository implements
+  AddAccountRepository.Protocol,
+  FindOneAccountByEmailRepository.Protocol
+{
   async add(accountData: AddAccountRepository.Request) {
     const {
       name,
@@ -15,6 +21,20 @@ export class PrismaAccountRepository implements AddAccountRepository.Protocol {
           name,
           email,
           password
+        }
+      }
+    );
+
+    return account;
+  }
+
+  async findOneByEmail(search: FindOneAccountByEmailRepository.Request) {
+    const { email } = search;
+
+    const account = await prisma.account.findFirst(
+      {
+        where: {
+          email
         }
       }
     );
