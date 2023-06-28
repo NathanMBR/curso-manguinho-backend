1ddaf31e88b5c2e6413b875a16ea4e21f52ac82c
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-06-28 11:31:06 -0300

    test: add find one by email method at prisma account repository

--- src/infra/db/prisma/repositories/PrismaAccountRepository.spec.ts
+++ src/infra/db/prisma/repositories/PrismaAccountRepository.spec.ts
@@ -20,7 +20,18 @@ const getSUTEnvironment = (): GetSUTEnvironmentResponse => {
   };
 };
 
-describe("Prisma Account Repository", () => {
+const returnAccountFunction: () => any = async () => Promise.resolve(
+  {
+    name: "Test Name",
+    email: "test@email.com",
+    password: "test1234"
+  }
+);
+
+jest.spyOn(prisma.account, "create").mockImplementation(returnAccountFunction);
+jest.spyOn(prisma.account, "findFirst").mockImplementation(returnAccountFunction);
+
+describe("Prisma AddAccount Repository", () => {
   it("should successfully add an account", async () => {
     const { SUT } = getSUTEnvironment();
 
@@ -30,28 +41,20 @@ describe("Prisma Account Repository", () => {
       password: "test1234"
     };
 
-    jest.spyOn(prisma.account, "create").mockImplementationOnce(
-      () => Promise.resolve(accountData) as any
-    );
-
-    const account = await SUT.add(accountData);
-    expect(account).toEqual(accountData);
+    const SUTResponse = await SUT.add(accountData);
+    expect(SUTResponse).toEqual(accountData);
   });
 
   it("should pass account data to prisma", async () => {
     const { SUT } = getSUTEnvironment();
+
+    const createSpy = jest.spyOn(prisma.account, "create");
 
     const accountData = {
       name: "Test Name",
       email: "test@email.com",
       password: "test1234"
     };
-
-    const createSpy = jest.spyOn(prisma.account, "create");
-
-    createSpy.mockImplementationOnce(
-      () => Promise.resolve(accountData) as any
-    );
 
     await SUT.add(accountData);
 
@@ -65,19 +68,74 @@ describe("Prisma Account Repository", () => {
   it("should repass prisma errors to upper level", async () => {
     const { SUT } = getSUTEnvironment();
 
-    const accountData = {
-      name: "Test Name",
-      email: "test@email.com",
-      password: "test1234"
-    };
-
     jest.spyOn(prisma.account, "create").mockImplementationOnce(
       () => {
         throw new Error();
       }
     );
 
+    const accountData = {
+      name: "Test Name",
+      email: "test@email.com",
+      password: "test1234"
+    };
+
     const SUTResponse = SUT.add(accountData);
+    await expect(SUTResponse).rejects.toThrow();
+  });
+});
+
+describe("Prisma FindOneAccountByEmail Repository", () => {
+  it("should successfully find an account by email", async () => {
+    const { SUT } = getSUTEnvironment();
+
+    const searchData = {
+      email: "search@email.com"
+    };
+
+    const accountData = {
+      name: "Test Name",
+      email: "test@email.com",
+      password: "test1234"
+    };
+
+    const SUTResponse = await SUT.findOneByEmail(searchData);
+    expect(SUTResponse).toEqual(accountData);
+  });
+
+  it("should pass search data to prisma", async () => {
+    const { SUT } = getSUTEnvironment();
+
+    const findFirstSpy = jest.spyOn(prisma.account, "findFirst");
+
+    const searchData = {
+      email: "search@email.com"
+    };
+    await SUT.findOneByEmail(searchData);
+
+    expect(findFirstSpy).toBeCalledWith(
+      {
+        where: {
+          email: searchData.email
+        }
+      }
+    );
+  });
+
+  it("should repass prisma errors to upper level", async () => {
+    const { SUT } = getSUTEnvironment();
+
+    jest.spyOn(prisma.account, "findFirst").mockImplementationOnce(
+      () => {
+        throw new Error()
+      }
+    );
+
+    const searchData = {
+      email: "search@email.com"
+    };
+  
+    const SUTResponse = SUT.findOneByEmail(searchData);
     await expect(SUTResponse).rejects.toThrow();
   });
 });
