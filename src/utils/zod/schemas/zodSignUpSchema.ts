1f8ecbe16b98e4c43e401073ae669184298917cf
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-06-30 17:08:01 -0300

    feat: add zod sign up schema

---
+++ src/utils/zod/schemas/zodSignUpSchema.ts
@@ -0,0 +1,42 @@
+import { z as zod } from "zod";
+
+export const zodSignUpSchema = zod.object(
+  {
+    name: zod
+      .string(
+        {
+          description: "The account name",
+          required_error: "The account name is required",
+          invalid_type_error: "The account name must be a string"
+        }
+      )
+      .min(3, "The account name must have at least 3 characters"),
+
+    email: zod
+      .string(
+        {
+          description: "The account email",
+          required_error: "The account email is required",
+          invalid_type_error: "The account email must be a string"
+        }
+      )
+      .max(255, "The account email must have at most 255 characters")
+      .email(),
+
+    password: zod
+      .string(
+        {
+          description: "The account password",
+          required_error: "The account password is required",
+          invalid_type_error: "The account password must be a string"
+        }
+      )
+      .min(8, "The account password must have at least 8 characters")
+  },
+
+  {
+    description: "The account payload",
+    required_error: "The account payload is required",
+    invalid_type_error: "The account payload must be an object"
+  }
+);
