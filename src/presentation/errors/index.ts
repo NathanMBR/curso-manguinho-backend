db9e9af3e9ca574e81ddc9bbaa21b2cd505939bb
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-06-30 17:35:59 -0300

    feat: add validation error

--- src/presentation/errors/index.ts
+++ src/presentation/errors/index.ts
@@ -2,3 +2,4 @@ export * from "./MissingParamError";
 export * from "./InvalidParamError";
 export * from "./InternalServerError";
 export * from "./EmailAlreadyExistsError";
+export * from "./ValidationError";
