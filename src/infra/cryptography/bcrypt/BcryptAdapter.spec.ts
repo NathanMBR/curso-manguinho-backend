71c9f028520f87f736660ac2d3d61aff575ef3d6
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-07-17 21:00:53 -0300

    test: add bcrypt adapter compare method

--- src/infra/cryptography/bcrypt/BcryptAdapter.spec.ts
+++ src/infra/cryptography/bcrypt/BcryptAdapter.spec.ts
@@ -9,9 +9,9 @@ import bcrypt from "bcrypt";
 import { BcryptAdapter } from "./BcryptAdapter";
 
 interface GetSUTEnvironmentResponse {
-  SUT: BcryptAdapter,
+  rounds: number;
 
-  rounds: number
+  SUT: BcryptAdapter;
 }
 
 const getSUTEnvironment = (): GetSUTEnvironmentResponse => {
@@ -19,35 +19,42 @@ const getSUTEnvironment = (): GetSUTEnvironmentResponse => {
   const SUT = new BcryptAdapter(rounds);
 
   return {
-    SUT,
+    rounds,
 
-    rounds
+    SUT
   };
 };
 
-describe("Bcrypt Adapter", () => {
+jest.spyOn(bcrypt, "hash").mockImplementation(
+  async () => Promise.resolve("stub-hash")
+);
+
+jest.spyOn(bcrypt, "compare").mockImplementation(
+  async () => Promise.resolve(true)
+);
+
+describe("Bcrypt Adapter Encrypt Method", () => {
   it("should successfully hash the value", async () => {
     const { SUT } = getSUTEnvironment();
 
-    jest.spyOn(bcrypt, "hash").mockImplementationOnce(
-      async () => Promise.resolve("hashed_value")
-    );
+    const SUTRequest = "test1234";
+    const SUTResponse = await SUT.encrypt(SUTRequest);
 
-    const SUTResponse = await SUT.encrypt("test1234");
-    expect(SUTResponse).toBe("hashed_value");
+    expect(SUTResponse).toBe("stub-hash");
   });
 
-  it("should pass string to bcrypt hash", async () => {
+  it("should pass text and rounds to bcrypt hash method", async () => {
     const { SUT, rounds } = getSUTEnvironment();
+
     const hashSpy = jest.spyOn(bcrypt, "hash");
-    const password = "test1234";
+    const SUTRequest = "test1234";
 
-    await SUT.encrypt(password);
+    await SUT.encrypt(SUTRequest);
 
-    expect(hashSpy).toHaveBeenCalledWith(password, rounds);
+    expect(hashSpy).toHaveBeenCalledWith(SUTRequest, rounds);
   });
 
-  it("should repass bcrypt errors to upper level", async () => {
+  it("should repass bcrypt hash method errors to upper level", async () => {
     const { SUT } = getSUTEnvironment();
 
     jest.spyOn(bcrypt, "hash").mockImplementationOnce(
@@ -56,7 +63,75 @@ describe("Bcrypt Adapter", () => {
       }
     );
 
-    const SUTResponse = SUT.encrypt("test1234");
+    const SUTRequest = "test1234";
+    const SUTResponse = SUT.encrypt(SUTRequest);
+
+    await expect(SUTResponse).rejects.toThrow();
+  });
+});
+
+describe("Bcrypt Adapter Compare Method", () => {
+  it("should successfully compare the value", async () => {
+    const { SUT } = getSUTEnvironment();
+
+    const SUTRequest = {
+      text: "test1234",
+      hash: "test-hash"
+    };
+
+    const SUTResponse = await SUT.compare(SUTRequest);
+
+    expect(SUTResponse).toBe(true);
+  });
+
+  it("should return false if bcrypt compare method returns false", async () => {
+    const { SUT } = getSUTEnvironment();
+
+    jest.spyOn(bcrypt, "compare").mockImplementationOnce(
+      async () => Promise.resolve(false)
+    );
+
+    const SUTRequest = {
+      text: "test1234",
+      hash: "test-hash"
+    };
+
+    const SUTResponse = await SUT.compare(SUTRequest);
+
+    expect(SUTResponse).toBe(false);
+  });
+
+  it("should pass text and hash to bcrypt compare method", async () => {
+    const { SUT } = getSUTEnvironment();
+
+    const compareSpy = jest.spyOn(bcrypt, "compare");
+
+    const SUTRequest = {
+      text: "test1234",
+      hash: "test-hash"
+    };
+
+    await SUT.compare(SUTRequest);
+
+    expect(compareSpy).toHaveBeenCalledWith(SUTRequest.text, SUTRequest.hash);
+  });
+
+  it("should repass bcrypt compare method errors to upper level", async () => {
+    const { SUT } = getSUTEnvironment();
+
+    jest.spyOn(bcrypt, "compare").mockImplementationOnce(
+      async () => {
+        throw new Error();
+      }
+    );
+
+    const SUTRequest = {
+      text: "test1234",
+      hash: "test-hash"
+    };
+
+    const SUTResponse = SUT.compare(SUTRequest);
+
     await expect(SUTResponse).rejects.toThrow();
   });
 });
