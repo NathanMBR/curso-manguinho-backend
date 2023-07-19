84ebb53bfd4de2f05fb7d5db70814a7d8944eb8c
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-07-18 23:03:50 -0300

    feat: add 'JWT_SECRET' environment variable to config

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,2 +1,3 @@
 export const FASTIFY_LOGGER = String(process.env.FASTIFY_LOGGER) === "true";
 export const PORT = Number(process.env.PORT) || 3000;
+export const JWT_SECRET = String(process.env.JWT_SECRET);
