792a84b6911ff51c60b92611e99adccb15593ed6
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-07-18 23:10:49 -0300

    feat: add make log in controller factory

--- src/main/factories/account/index.ts
+++ src/main/factories/account/index.ts
@@ -1,1 +1,2 @@
 export * from "./makeSignUpController";
+export * from "./makeLogInController";
---
+++ src/main/factories/account/makeLogInController.ts
@@ -0,0 +1,53 @@
+import {
+  DbFindOneAccountByEmail,
+  DbCompareAccountPassword,
+  DbAuthenticateAccount
+} from "../../../data/usecases";
+import {
+  BcryptAdapter,
+  JWTAdapter
+} from "../../../infra/cryptography";
+import {
+  bcryptHashRounds,
+  JWT_SECRET,
+  jwtExpirationTime
+} from "../../config";
+import { LogInValidatorAdapter } from "../../../validation/adapters";
+import { LogInController } from "../../../presentation/controllers";
+import { PrismaAccountRepository } from "../../../infra/db";
+import { ZodLogInValidator } from "../../../infra/validators";
+import { PinoLoggerAdapter } from "../../../infra/log";
+import { ErrorHandlerControllerDecorator } from "../../decorators";
+
+export const makeLogInController = () => {
+  const validator = new ZodLogInValidator();
+  const validatorAdapter = new LogInValidatorAdapter(validator);
+
+  const encrypter = new BcryptAdapter(bcryptHashRounds);
+  const tokenSigner = new JWTAdapter(
+    JWT_SECRET,
+    jwtExpirationTime
+  );
+
+  const accountRepository = new PrismaAccountRepository();
+
+  const dbFindOneAccountByEmail = new DbFindOneAccountByEmail(accountRepository);
+  const dbCompareAccountPassword = new DbCompareAccountPassword(encrypter);
+  const dbAuthenticateAccount = new DbAuthenticateAccount(tokenSigner);
+
+  const logInController = new LogInController(
+    validatorAdapter,
+    dbFindOneAccountByEmail,
+    dbCompareAccountPassword,
+    dbAuthenticateAccount
+  );
+
+  const logger = new PinoLoggerAdapter();
+
+  const errorHandlerDecorator = new ErrorHandlerControllerDecorator(
+    logInController,
+    logger
+  );
+
+  return errorHandlerDecorator;
+};
