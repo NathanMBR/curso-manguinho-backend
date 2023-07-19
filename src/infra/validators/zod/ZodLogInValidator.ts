a69f46f43ada80793790eca85ecae51308796367
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-07-18 22:42:15 -0300

    feat: add zod log in validator

---
+++ src/infra/validators/zod/ZodLogInValidator.ts
@@ -0,0 +1,12 @@
+import { LogInValidator } from "../../../validation/protocols";
+import { zodLogInSchema } from "./schemas";
+import { zodValidationAdapter } from "./adapters";
+
+export class ZodLogInValidator implements LogInValidator.Protocol {
+  validate(data: LogInValidator.Request): LogInValidator.Response {
+    const zodValidation = zodLogInSchema.safeParse(data);
+
+    const validationResponse = zodValidationAdapter(zodValidation);
+    return validationResponse;
+  }
+}
--- src/infra/validators/zod/index.ts
+++ src/infra/validators/zod/index.ts
@@ -1,1 +1,2 @@
 export * from "./ZodSignUpValidator";
+export * from "./ZodLogInValidator";
