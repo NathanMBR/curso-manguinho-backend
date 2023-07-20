43f40b876b2312f463aaff3f170743f4f6d0334a
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-07-20 14:34:11 -0300

    feat: add type property to repository account model

--- src/data/models/RepositoryAccount.ts
+++ src/data/models/RepositoryAccount.ts
@@ -1,6 +1,9 @@
+export type RepositoryAccountType = "COMMON" | "ADMIN";
+
 export interface RepositoryAccount {
   id: string;
   name: string;
   email: string;
   password: string;
+  type: RepositoryAccountType;
 };
