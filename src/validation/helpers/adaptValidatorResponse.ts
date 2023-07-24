6a0f4bca0a46094613cb77413e1272d401096da4
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-07-24 20:29:36 -0300

    feat: make adapt validator response helper return data property

--- src/validation/helpers/adaptValidatorResponse.ts
+++ src/validation/helpers/adaptValidatorResponse.ts
@@ -1,21 +1,28 @@
 import { Validator } from "../../presentation/protocols";
 import { ValidationResponse } from "../models";
 
-export const adaptValidatorResponse = (validationResponse: ValidationResponse): Validator.Response => {
-  const { isValid } = validationResponse;
+export const adaptValidatorResponse = <T>(validationResponse: ValidationResponse<T>): Validator.Response<T> => {
+  const { isValid, data } = validationResponse;
   const [errorMessage] = validationResponse.errors;
 
   if (isValid) {
     if (errorMessage)
       throw new Error("Unexpected error message to adapt");
 
+    if (!data)
+      throw new Error("Missing validated data");
+
     return {
-      isValid
+      isValid,
+      data
     };
   }
 
   if (!errorMessage)
     throw new Error("Missing error message to adapt");
+
+  if (data)
+    throw new Error("Unexpected data with failed validation");
 
   return {
     isValid,
