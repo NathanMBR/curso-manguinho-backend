7e04d070714080947d9fd6440244cf445ae3a146
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-07-24 20:30:32 -0300

    feat: make zod validaton adapter return data property

--- src/infra/validators/zod/adapters/zodValidationAdapter.ts
+++ src/infra/validators/zod/adapters/zodValidationAdapter.ts
@@ -1,17 +1,19 @@
-import { SafeParseReturnType } from "zod";
+import { SafeParseReturnType, z } from "zod";
 
 import { ValidationResponse } from "../../../../validation/models";
 
-export const zodValidationAdapter = <T, K>(zodValidationResponse: SafeParseReturnType<T, K>): ValidationResponse => {
+export const zodValidationAdapter = <T, K>(zodValidationResponse: SafeParseReturnType<T, K>) => {
   const { success } = zodValidationResponse;
 
-  const adaptedResponse: ValidationResponse = {
+  const adaptedResponse: ValidationResponse<K> = {
     isValid: success,
     errors: []
   };
 
   if (!success)
     adaptedResponse.errors = zodValidationResponse.error.issues.map(issue => issue.message);
+  else
+    adaptedResponse.data = zodValidationResponse.data;
 
   return adaptedResponse;
 };
