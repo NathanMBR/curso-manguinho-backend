c6f53432b93b7a694a5b8e62ddbb5b44febb0d11
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-07-24 17:16:09 -0300

    feat: add data property to validator presentation protocol response

--- src/presentation/protocols/Validator.ts
+++ src/presentation/protocols/Validator.ts
@@ -1,14 +1,15 @@
 export namespace Validator {
   export type Request = unknown;
 
-  export type Response = {
+  export type Response<T> = {
     isValid: false;
     errorMessage: string;
   } | {
     isValid: true;
+    data: T;
   };
 
-  export interface Protocol {
-    validate: (data: Validator.Request) => Validator.Response;
+  export interface Protocol<T> {
+    validate: (data: Validator.Request) => Validator.Response<T>;
   }
 }
