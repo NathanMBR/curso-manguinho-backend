f95f1baf1cf356ac2f0f260fabe88393886d82a5
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-07-26 12:06:22 -0300

    feat: add make add survey controller factory

---
+++ src/main/factories/survey/makeAddSurveyController.ts
@@ -0,0 +1,30 @@
+import { DbAddSurvey } from "../../../data/usecases";
+import { ZodAddSurveyValidator } from "../../../infra/validators";
+import { AddSurveyValidatorAdapter } from "../../../validation/adapters";
+import { PrismaSurveyRepository } from "../../../infra/db";
+import { AddSurveyController } from "../../../presentation/controllers";
+import { PinoLoggerAdapter } from "../../../infra/log";
+import { ErrorHandlerControllerDecorator } from "../../decorators";
+
+export const makeAddSurveyController = () => {
+  const validator = new ZodAddSurveyValidator();
+  const validatorAdapter = new AddSurveyValidatorAdapter(validator);
+
+  const surveyRepository = new PrismaSurveyRepository();
+
+  const dbAddSurvey = new DbAddSurvey(surveyRepository);
+
+  const addSurveyController = new AddSurveyController(
+    validatorAdapter,
+    dbAddSurvey
+  );
+
+  const logger = new PinoLoggerAdapter();
+
+  const errorHandlerDecorator = new ErrorHandlerControllerDecorator(
+    addSurveyController,
+    logger
+  );
+
+  return errorHandlerDecorator;
+};
