4c796915b3b1d047e67946688b79236d11b90d23
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-07-27 17:36:38 -0300

    refactor: make http response helper import http response

--- src/presentation/helpers/HttpResponseHelper.ts
+++ src/presentation/helpers/HttpResponseHelper.ts
@@ -1,31 +1,31 @@
-import { Controller } from "../protocols";
+import { HttpResponse } from "../models";
 
 export abstract class HttpResponseHelper {
-  public static ok(body: any): Controller.HttpResponse {
-    return new Controller.HttpResponse(200, body);
+  public static ok(body: any): HttpResponse {
+    return new HttpResponse(200, body);
   }
 
-  public static created(body: any): Controller.HttpResponse {
-    return new Controller.HttpResponse(201, body);
+  public static created(body: any): HttpResponse {
+    return new HttpResponse(201, body);
   }
 
-  public static noContent(): Controller.HttpResponse {
-    return new Controller.HttpResponse(204, null);
+  public static noContent(): HttpResponse {
+    return new HttpResponse(204, null);
   }
 
-  public static badRequest(error: Error): Controller.HttpResponse {
-    return new Controller.HttpResponse(400, error);
+  public static badRequest(error: Error): HttpResponse {
+    return new HttpResponse(400, error);
   }
 
-  public static unauthorized(error: Error): Controller.HttpResponse {
-    return new Controller.HttpResponse(401, error);
+  public static unauthorized(error: Error): HttpResponse {
+    return new HttpResponse(401, error);
   }
 
-  public static notFound(error: Error): Controller.HttpResponse {
-    return new Controller.HttpResponse(404, error);
+  public static notFound(error: Error): HttpResponse {
+    return new HttpResponse(404, error);
   }
 
-  public static internalServerError(error: Error): Controller.HttpResponse {
-    return new Controller.HttpResponse(500, error);
+  public static internalServerError(error: Error): HttpResponse {
+    return new HttpResponse(500, error);
   }
 }
