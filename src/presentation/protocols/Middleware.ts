2371c16bcb158feba0fed58917577f8aede23199
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-07-27 17:57:18 -0300

    refactor: change response type at middleware protocol

--- src/presentation/protocols/Middleware.ts
+++ src/presentation/protocols/Middleware.ts
@@ -6,15 +6,17 @@ import {
 export namespace Middleware {
   export type Request = HttpRequest;
 
-  export type Response = {
-    success: true;
-    httpRequest: HttpRequest;
-  } | {
-    success: false;
-    httpResponse: HttpResponse;
-  }
+  export type Response = Promise<
+    {
+      success: true;
+      httpRequest: HttpRequest;
+    } | {
+      success: false;
+      httpResponse: HttpResponse;
+    }
+  >;
 
   export interface Protocol {
-    handle: (httpRequest: Middleware.Request) => Promise<Middleware.Response>;
+    handle(httpRequest: Middleware.Request): Middleware.Response;
   }
 }
