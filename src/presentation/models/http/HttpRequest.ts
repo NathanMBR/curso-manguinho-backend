35abf8558e11bc87d7c13f8850e7a313a616ac52
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-07-28 16:39:41 -0300

    feat: add headers property to http request model

--- src/presentation/models/http/HttpRequest.ts
+++ src/presentation/models/http/HttpRequest.ts
@@ -1,7 +1,10 @@
 export interface HttpRequest {
+  headers?: {
+    authorization?: string;
+  };
   body?: any;
   authenticationData?: {
     id: string;
     type: "COMMON" | "ADMIN";
-  }
+  };
 }
