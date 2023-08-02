c5e5cb4a7a57a1587b349192d1ee37b3ccd4ec4b
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-08-02 20:05:54 -0300

    feat: add db find many surveys use case

---
+++ src/data/usecases/survey/DbFindManySurveys.ts
@@ -0,0 +1,40 @@
+import { FindManySurveys } from "../../../domain/usecases";
+import {
+  FindManySurveysRepository,
+  CountManySurveysRepository
+} from "../../protocols";
+import {
+  calculateRepositoryPaginationParameters,
+  getRepositoryPaginationResponse
+} from "../../helpers";
+
+export class DbFindManySurveys implements FindManySurveys.Protocol {
+  constructor(
+    private readonly findManySurveysRepository: FindManySurveysRepository.Protocol,
+    private readonly countManySurveysRepository: CountManySurveysRepository.Protocol
+  ) {}
+
+  async findMany(request: FindManySurveys.Request): FindManySurveys.Response {
+    const repositoryPaginationParameters = calculateRepositoryPaginationParameters(request);
+
+    const [
+      surveys,
+      count
+    ] = await Promise.all(
+      [
+        this.findManySurveysRepository.findMany(repositoryPaginationParameters),
+        this.countManySurveysRepository.countMany(repositoryPaginationParameters)
+      ]
+    );
+
+    const repositoryPaginationResponse = getRepositoryPaginationResponse(
+      {
+        ...repositoryPaginationParameters,
+        count,
+        data: surveys
+      }
+    );
+
+    return repositoryPaginationResponse;
+  }
+}
