0d11d916d74edbd4b65d0b3837cc3cb587177d6b
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-08-07 10:18:28 -0300

    feat: add zod find many surveys schema

--- src/infra/validators/zod/schemas/survey/index.ts
+++ src/infra/validators/zod/schemas/survey/index.ts
@@ -1,1 +1,2 @@
 export * from "./zodAddSurveySchema";
+export * from "./zodFindManySurveysSchema";
---
+++ src/infra/validators/zod/schemas/survey/zodFindManySurveysSchema.ts
@@ -0,0 +1,34 @@
+import { z as zod } from "zod";
+
+export const zodFindManySurveysSchema = zod.object(
+  {
+    page: zod
+      .number(
+        {
+          description: "The surveys page",
+          required_error: "The surveys page is required",
+          invalid_type_error: "The surveys page must be a number"
+        }
+      )
+      .min(1, "The surveys page must be greater than or equal to 1")
+      .int("The surveys page must be an integer number"),
+
+    quantity: zod
+      .number(
+        {
+          description: "The surveys quantity",
+          required_error: "The surveys quantity is required",
+          invalid_type_error: "The surveys quantity must be a number"
+        }
+      )
+      .min(1, "The surveys quantity must be greater than or equal to 1")
+      .max(50, "The surveys quantity must be less than or equal to 50")
+      .int("The surveys quantity must be an integer number")
+  },
+
+  {
+    description: "The find many surveys payload",
+    required_error: "The find many surveys payload is required",
+    invalid_type_error: "The find many surveys payload must be an object"
+  }
+);
