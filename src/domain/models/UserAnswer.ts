4ee9b2a8b4be945683f3a18d75925c44bfacef00
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-08-10 12:53:25 -0300

    feat: add user answer model

---
+++ src/domain/models/UserAnswer.ts
@@ -0,0 +1,6 @@
+export interface UserAnswer {
+  id: string;
+  accountId: string;
+  questionId: string;
+  answerId: string;
+}
--- src/domain/models/index.ts
+++ src/domain/models/index.ts
@@ -2,5 +2,6 @@ export * from "./Account";
 export * from "./Answer";
 export * from "./Question";
 export * from "./Survey";
+export * from "./UserAnswer";
 
 export * from "./Pagination";
