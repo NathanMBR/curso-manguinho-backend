fe94bcf6eacdd7a373141d0e37748415eff49123
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-08-11 11:40:18 -0300

    test: add db add user answer use case

---
+++ src/data/usecases/userAnswer/DbAddUserAnswer.spec.ts
@@ -0,0 +1,103 @@
+import {
+  describe,
+  it,
+  expect,
+  jest
+} from "@jest/globals";
+
+import { AddUserAnswerRepository } from "../../protocols";
+import { DbAddUserAnswer } from "./DbAddUserAnswer";
+
+interface GetSUTEnvironmentResponse {
+  addUserAnswerRepository: AddUserAnswerRepository.Protocol;
+
+  SUT: DbAddUserAnswer;
+}
+
+const getSUTEnvironment = (): GetSUTEnvironmentResponse => {
+  class AddUserAnswerRepositoryStub implements AddUserAnswerRepository.Protocol {
+    async add(_request: AddUserAnswerRepository.Request): AddUserAnswerRepository.Response {
+      return Promise.resolve(
+        {
+          id: "test-user-answer-id",
+          accountId: "test-account-id",
+          questionId: "test-question-id",
+          answerId: "test-answer-id",
+        }
+      );
+    }
+  }
+
+  const addUserAnswerRepository = new AddUserAnswerRepositoryStub();
+
+  const dbAddUserAnswer = new DbAddUserAnswer(
+    addUserAnswerRepository
+  );
+
+  return {
+    addUserAnswerRepository,
+
+    SUT: dbAddUserAnswer
+  };
+};
+
+describe("DbAddUserAnswer UseCase", () => {
+  it("should successfully add a survey", async () => {
+    const { SUT } = getSUTEnvironment();
+
+    const SUTRequest = {
+      accountId: "test-account-id",
+      questionId: "test-question-id",
+      answerId: "test-answer-id"
+    };
+
+    const SUTResponse = await SUT.add(SUTRequest);
+
+    const expectedResponse = {
+      id: "test-user-answer-id",
+      accountId: "test-account-id",
+      questionId: "test-question-id",
+      answerId: "test-answer-id",
+    };
+
+    expect(SUTResponse).toEqual(expectedResponse);
+  });
+
+  it("should pass user answer data to add user answer repository call", async () => {
+    const { SUT, addUserAnswerRepository } = getSUTEnvironment();
+
+    const addSpy = jest.spyOn(addUserAnswerRepository, "add");
+
+    const SUTRequest = {
+      accountId: "test-account-id",
+      questionId: "test-question-id",
+      answerId: "test-answer-id"
+    };
+
+    await SUT.add(SUTRequest);
+
+    const expectedCall = SUTRequest;
+
+    expect(addSpy).toHaveBeenCalledWith(expectedCall);
+  });
+
+  it("should repass add user answer repository errors to upper level", async () => {
+    const { SUT, addUserAnswerRepository } = getSUTEnvironment();
+
+    jest.spyOn(addUserAnswerRepository, "add").mockImplementationOnce(
+      async () => {
+        throw new Error("Test error");
+      }
+    );
+
+    const SUTRequest = {
+      accountId: "test-account-id",
+      questionId: "test-question-id",
+      answerId: "test-answer-id"
+    };
+
+    const SUTResponse = SUT.add(SUTRequest);
+
+    await expect(SUTResponse).rejects.toThrow();
+  });
+});
