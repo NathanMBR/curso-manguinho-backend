3e4b2cf468a71feed87430637b6bab18bb2408c7
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-08-12 01:57:17 -0300

    refactor: make add user answer use case return successfull and failure responses

--- src/domain/usecases/userAnswer/AddUserAnswer.ts
+++ src/domain/usecases/userAnswer/AddUserAnswer.ts
@@ -1,12 +1,27 @@
-import { UserAnswer } from "../../models";
+import {
+  UserAnswer,
+  Survey
+} from "../../models";
 
 export namespace AddUserAnswer {
   export type Request = {
+    survey: Survey;
     accountId: string;
     userAnswers: Array<Omit<UserAnswer, "id" | "accountId">>;
   };
 
-  export type Response = Promise<void>;
+  type SuccessfulResponse = {
+    success: true;
+  };
+
+  type FailureResponse = {
+    success: false;
+    errorMessage: string;
+  };
+
+  type PossibleResponses = SuccessfulResponse | FailureResponse;
+
+  export type Response = Promise<PossibleResponses>;
 
   export interface Protocol {
     add(request: AddUserAnswer.Request): AddUserAnswer.Response;
