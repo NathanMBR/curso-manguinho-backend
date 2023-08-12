07316d4e487b232c366aa49e3bd5ef2422bdfd79
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-08-12 12:58:48 -0300

    feat: ensure db add user answer rejects inconsistent ids

--- src/data/usecases/userAnswer/DbAddUserAnswer.ts
+++ src/data/usecases/userAnswer/DbAddUserAnswer.ts
@@ -7,8 +7,101 @@ export class DbAddUserAnswer implements AddUserAnswer.Protocol {
   ) {}
 
   async add(request: AddUserAnswer.Request): AddUserAnswer.Response {
-    const userAnswer = await this.addUserAnswerRepository.add(request);
+    const {
+      survey,
+      accountId,
+      userAnswers
+    } = request;
 
-    return userAnswer;
+    const doesEveryQuestionBelongsToSurvey = userAnswers.every(
+      userAnswer => survey.questions.some(
+        question => question.id === userAnswer.questionId
+      )
+    );
+
+    if (!doesEveryQuestionBelongsToSurvey)
+      return {
+        success: false,
+        errorMessage: "Some of the questions don't belong to the survey"
+      };
+
+    const doesEveryAnswerBelongsToItsQuestion = userAnswers.every(
+      userAnswer => survey.questions.find(
+        question => question.id === userAnswer.questionId
+      )!.answers.some(
+        answer => answer.id === userAnswer.answerId
+      )
+    );
+
+    if (!doesEveryAnswerBelongsToItsQuestion)
+      return {
+        success: false,
+        errorMessage: "Some of the answers don't belong to its question"
+      };
+
+    const doesEveryQuestionHasAtLeastOneUserAnswer = survey.questions.every(
+      question => userAnswers.some(
+        userAnswer => userAnswer.questionId === question.id
+      )
+    );
+
+    if (!doesEveryQuestionHasAtLeastOneUserAnswer)
+      return {
+        success: false,
+        errorMessage: "Some of the questions doesn't have at least one user answer"
+      };
+
+    const doesSomeSingleTypeQuestionHasMoreThanOneUserAnswer = survey.questions
+      .filter(question => question.type === "SINGLE")
+      .some(
+        question => {
+          let count = 0;
+
+          for (const userAnswer of userAnswers) {
+            if (userAnswer.questionId === question.id)
+              count++;
+          }
+
+          return count > 1;
+        }
+      );
+
+    if (doesSomeSingleTypeQuestionHasMoreThanOneUserAnswer)
+      return {
+        success: false,
+        errorMessage: "Some of the single type questions has more than one user answer"
+      };
+
+    const doesDuplicatedUserAnswerExists = userAnswers.some(
+      (userAnswer, index) => {
+        for (let i = index + 1; i < userAnswers.length; i++) {
+          const anotherUserAnswer = userAnswers[i]!;
+          const isQuestionIdDuplicated = anotherUserAnswer.questionId === userAnswer.questionId;
+          const isAnswerIdDuplicated = anotherUserAnswer.answerId === userAnswer.answerId;
+
+          if (isQuestionIdDuplicated && isAnswerIdDuplicated)
+            return true;
+        }
+
+        return false;
+      }
+    );
+
+    if (doesDuplicatedUserAnswerExists)
+      return {
+        success: false,
+        errorMessage: "Duplicated user answer"
+      };
+
+    await this.addUserAnswerRepository.add(
+      {
+        accountId,
+        userAnswers
+      }
+    );
+
+    return {
+      success: true
+    };
   }
 }
