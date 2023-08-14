1b754e0e3368ad550e870fca04611cc49db621e9
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-08-14 17:32:57 -0300

    feat: add repository user answered survey model

---
+++ src/data/models/RepositoryUserAnsweredSurvey.ts
@@ -0,0 +1,5 @@
+export interface RepositoryUserAnsweredSurvey {
+  id: string;
+  accountId: string;
+  surveyId: string;
+}
--- src/data/models/index.ts
+++ src/data/models/index.ts
@@ -3,5 +3,6 @@ export * from "./RepositoryAnswer";
 export * from "./RepositoryQuestion";
 export * from "./RepositorySurvey";
 export * from "./RepositoryUserAnswer";
+export * from "./RepositoryUserAnsweredSurvey";
 
 export * from "./RepositoryPagination";
