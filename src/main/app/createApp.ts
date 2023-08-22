9a3f78104e43610afeffe7f70f68d4e04b64dac6
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-08-21 21:11:43 -0300

    feat: inject inject apollo server routes at create app

--- src/main/app/createApp.ts
+++ src/main/app/createApp.ts
@@ -2,7 +2,10 @@ import fastify from "fastify";
 import cors from "@fastify/cors";
 
 import { FASTIFY_LOGGER } from "../config";
-import { fastifyRoutes } from "./routes";
+import {
+  fastifyRoutes,
+  injectApolloServerRoutes
+} from "./routes";
 
 export const createApp = async () => {
   const app = fastify(
@@ -13,6 +16,7 @@ export const createApp = async () => {
 
   await app.register(cors);
   await app.register(fastifyRoutes, { prefix: "/api" });
+  await injectApolloServerRoutes(app);
 
   return app;
 };
