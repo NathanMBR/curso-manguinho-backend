d0f45f50b4e7ef1e509f315291c1609ebc08b3d9
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-08-21 21:07:38 -0300

    feat: add 'APOLLO_SERVER_STACKTRACE' environment variable to config

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,4 @@
 export const FASTIFY_LOGGER = String(process.env.FASTIFY_LOGGER) === "true";
+export const APOLLO_SERVER_STACKTRACE = String(process.env.APOLLO_SERVER_STACKTRACE) === "true";
 export const PORT = Number(process.env.PORT) || 3000;
 export const JWT_SECRET = String(process.env.JWT_SECRET);
