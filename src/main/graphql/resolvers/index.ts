d09ebb19ea47c69cc29833dc4261343cf258d2b0
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-08-23 17:14:08 -0300

    feat: add graphql survey resolvers

--- src/main/graphql/resolvers/index.ts
+++ src/main/graphql/resolvers/index.ts
@@ -1,1 +1,2 @@
 export * from "./accountGQLResolvers";
+export * from "./surveyGQLResolvers";
---
+++ src/main/graphql/resolvers/surveyGQLResolvers.ts
@@ -0,0 +1,108 @@
+import {
+  makeFindManySurveysController,
+  makeFindOneSurveyController,
+  makeGetSurveyResultController,
+  makeAddSurveyController,
+  makeAddSurveyAnswerController,
+
+  makeAuthenticationMiddleware,
+  makeAdminMiddleware
+} from "../../factories";
+
+import { apolloServerResolverAdapter } from "../../adapters";
+import { Resolver } from "../protocols";
+
+
+export const surveyGQLResolvers: Resolver = {
+  Query: {
+    surveys: async (_parent, args, ctx) => {
+      const findManySurveysController = makeFindManySurveysController();
+      const authenticationMiddleware = makeAuthenticationMiddleware();
+
+      const httpResponseBody = await apolloServerResolverAdapter(
+        {
+          query: args.query,
+          headers: ctx.headers
+        },
+        findManySurveysController,
+        authenticationMiddleware
+      );
+
+      return httpResponseBody;
+    },
+
+    survey: async (_parent, args, ctx) => {
+      const findOneSurveyController = makeFindOneSurveyController();
+      const authenticationMiddleware = makeAuthenticationMiddleware();
+
+      const httpResponseBody = await apolloServerResolverAdapter(
+        {
+          params: {
+            id: args.id
+          },
+          headers: ctx.headers
+        },
+        findOneSurveyController,
+        authenticationMiddleware
+      );
+
+      return httpResponseBody;
+    },
+
+    surveyResult: async (_parent, args, ctx) => {
+      const getSurveyResultController = makeGetSurveyResultController();
+      const authenticationMiddleware = makeAuthenticationMiddleware();
+      const adminMiddleware = makeAdminMiddleware();
+
+      const httpResponseBody = await apolloServerResolverAdapter(
+        {
+          params: {
+            id: args.id
+          },
+          headers: ctx.headers
+        },
+        getSurveyResultController,
+        authenticationMiddleware,
+        adminMiddleware
+      );
+
+      return httpResponseBody;
+    }
+  },
+
+  Mutation: {
+    addSurvey: async (_parent, args, ctx) => {
+      const addSurveyController = makeAddSurveyController();
+      const authenticationMiddleware = makeAuthenticationMiddleware();
+      const adminMiddleware = makeAdminMiddleware();
+
+      const httpResponseBody = await apolloServerResolverAdapter(
+        {
+          body: args.body,
+          headers: ctx.headers
+        },
+        addSurveyController,
+        authenticationMiddleware,
+        adminMiddleware
+      );
+
+      return httpResponseBody;
+    },
+
+    addSurveyAnswer: async (_parent, args, ctx) => {
+      const addSurveyAnswerController = makeAddSurveyAnswerController();
+      const authenticationMiddleware = makeAuthenticationMiddleware();
+
+      const httpResponseBody = await apolloServerResolverAdapter(
+        {
+          body: args.body,
+          headers: ctx.headers
+        },
+        addSurveyAnswerController,
+        authenticationMiddleware
+      );
+
+      return httpResponseBody;
+    }
+  }
+};
